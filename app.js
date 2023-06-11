ce22d24728ebd1885687cb66ef5766921284e05c
Jonathan Dominguez <jonathan-g-dominguez@outlook.es>
2023-06-11 20:41:52 -0300

    Primer test de JS agregado de condicon y total de ticket con descuento

--- app.js
+++
@@ -1,5 +0,0 @@
-import {prueba1} from './tickets.js';
-
-console.log("Hola mundo");
-
-prueba1();
--- tickets.js
+++ tickets.js
@@ -1,68 +1,24 @@
-// const prueba1 = ()=>{
-//     console.log("Probando importacion");
-// }
+const inputCantidad = document.querySelector("#inputCantidad").value;
+const divTotalTickets = document.querySelector("#divTotalTickets");
+const selectCategorias = document.querySelector("#inputOpciones");
+const btnSubmit = document.querySelector("#btnSubmit");
+const valorTicket = 2000;
+const descEstudiante = 0.2;
+const descTrainee = 0.5;
+const descJr = 0.85;
 
-// prueba1();
-// let n1 = parseInt(prompt("Ingresa un numero para sumar"));
-// let n2 = parseInt(prompt("Ingresa otro numero para sumar"))
-// const sumando = (n1,n2)=>{
-//     return n1+n2;
-// }
 
-// const resultado = sumando(n1,n2);
-// alert(`Tu resultado es ${resultado}`);
+const calculoValores = ()=>{
 
-// const mayorMenor = (resultado)=>{
-//     if (resultado>10) {
-//         return console.log("Es mayor a 10");
-//     }
-//     return console.log("Es menor a 10");
-// }
+    const categoriaSeleccionada = selectCategorias.value;
+    if(categoriaSeleccionada === "estudiante"){
+        let cuenta = valorTicket*descEstudiante;
+        divTotalTickets.innerHTML = `<h2>Total de tickets: $${cuenta}</h2>`;
+        console.log("Probando");
+    }
+}
 
-// mayorMenor(resultado);
-
-// const nacimiento = parseInt(prompt("Ingresa tu fecha de nacimiento YYYY"));
-
-// const prueba = (nacimiento)=>{
-//     return (2023 - nacimiento);
-// }
-
-// console.log(prueba(nacimiento));
-
-// const peso = parseFloat(prompt("Ingresa tu peso en libras"));
-// const cuenta = (peso)=>{
-//     return (peso/2.205);
-// }
-// const pesoRound = cuenta(peso);
-// console.log(`Su peso en Kg es: ${Math.round(pesoRound)}`);
-
-// const sueldo = parseInt(prompt("Ingrese su sueldo:"));
-
-// const cuenta = (sueldo)=>{
-//     const multi = sueldo*1.15;
-//     return multi
-// }
-
-// console.log(`Su sueldo es ${cuenta(sueldo)}`);
-
-// const nota = parseInt(prompt("Ingrese la nota"));
-
-// const program = (nota)=>{
-//     nota>=7?alert("Aprobado"):alert("Desaprobado");
-// }
-
-// program(nota);
-
-// const matricula = prompt("Ingrese matrícula");
-
-// const calificaciones = []
-
-// const program = ()=>{
-//     for (let index = 1; index <= 5; index++) {
-//         const calificacion = parseInt(prompt("Ingrese la nota numero " + index));
-//         calificaciones.push(calificacion)
-//     }
-//     return calificaciones;
-// }
-
-// console.log(program());
+btnSubmit.addEventListener('click',(e)=>{
+    e.preventDefault();
+})
+btnSubmit.addEventListener('click',calculoValores);
