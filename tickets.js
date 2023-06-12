e793326a7eb00f448d3fc0f44cc5130184ef8a5e
Jonathan Dominguez <jonathan-g-dominguez@outlook.es>
2023-06-11 22:28:11 -0300

    Agregado de la opción sin categoria y agregado de estilo

--- tickets.js
+++ tickets.js
@@ -3,29 +3,42 @@ const divTotalTickets = document.querySelector("#divTotalTickets");
 const selectCategorias = document.querySelector("#inputOpciones");
 const btnSubmit = document.querySelector("#btnSubmit");
 const cantidadTickets = document.querySelector("#inputCantidad");
+
 const valorTicket = 2000;
 const descEstudiante = 0.2;
 const descTrainee = 0.5;
 const descJr = 0.85;
 
+const validacionCampos = ()=>{
+    const inputNombre = document.querySelector("#inputNombre");
+    const inputApellido = document.querySelector("#inputApellido");
+    const inputEmail = document.querySelector("#inputEmail");
+
+    if(isNaN(inputNombre) === true){
+
+    }
+}
 
 const calculoValores = ()=>{
 
     const categoriaSeleccionada = selectCategorias.value;
     let cantidadSeleccionada = inputCantidad.value;
     if(cantidadSeleccionada > 0){
+        if(categoriaSeleccionada === "sinCategoria"){
+            let cuenta = valorTicket*cantidadSeleccionada;
+            divTotalTickets.innerHTML = `<h2 class="muestraValorTickets">Total de tickets: $${cuenta}</h2>`;
+        }
         if(categoriaSeleccionada === "estudiante"){
             let cuenta = (valorTicket*descEstudiante)*cantidadSeleccionada;
-            divTotalTickets.innerHTML = `<h2>Total de tickets: $${cuenta}</h2>`;
-            console.log("Probando");
+            divTotalTickets.innerHTML = `<h2 class="muestraValorTickets">Total de tickets: $${cuenta}</h2>`;
         }
         if(categoriaSeleccionada==="trainee"){
             let cuenta = (valorTicket*descTrainee)*cantidadSeleccionada;
-            divTotalTickets.innerHTML = `<h2>Total de tickets: $${cuenta}</h2>`;
+            divTotalTickets.innerHTML = `<h2 class="muestraValorTickets">Total de tickets: $${cuenta}</h2>`;
         }
         if(categoriaSeleccionada==="junior"){
             let cuenta = (valorTicket*descJr)*cantidadSeleccionada;
-            divTotalTickets.innerHTML = `<h2>Total de tickets: $${cuenta}</h2>`;
+            divTotalTickets.innerHTML = `<h2 class="muestraValorTickets text-center">Total de tickets: $${cuenta}</h2>`;
         }
     }
     else{
