c532629d0ade6346175c55cd7126c784e3cd2d49
Jonathan Dominguez <jonathan-g-dominguez@outlook.es>
2023-06-11 22:02:26 -0300

    Agregado de validación para la cantidad

--- tickets.js
+++ tickets.js
@@ -1,7 +1,8 @@
-const inputCantidad = document.querySelector("#inputCantidad").value;
+const inputCantidad = document.querySelector("#inputCantidad");
 const divTotalTickets = document.querySelector("#divTotalTickets");
 const selectCategorias = document.querySelector("#inputOpciones");
 const btnSubmit = document.querySelector("#btnSubmit");
+const cantidadTickets = document.querySelector("#inputCantidad");
 const valorTicket = 2000;
 const descEstudiante = 0.2;
 const descTrainee = 0.5;
@@ -11,12 +12,27 @@ const descJr = 0.85;
 const calculoValores = ()=>{
 
     const categoriaSeleccionada = selectCategorias.value;
-    if(categoriaSeleccionada === "estudiante"){
-        let cuenta = valorTicket*descEstudiante;
-        divTotalTickets.innerHTML = `<h2>Total de tickets: $${cuenta}</h2>`;
-        console.log("Probando");
+    let cantidadSeleccionada = inputCantidad.value;
+    if(cantidadSeleccionada > 0){
+        if(categoriaSeleccionada === "estudiante"){
+            let cuenta = (valorTicket*descEstudiante)*cantidadSeleccionada;
+            divTotalTickets.innerHTML = `<h2>Total de tickets: $${cuenta}</h2>`;
+            console.log("Probando");
+        }
+        if(categoriaSeleccionada==="trainee"){
+            let cuenta = (valorTicket*descTrainee)*cantidadSeleccionada;
+            divTotalTickets.innerHTML = `<h2>Total de tickets: $${cuenta}</h2>`;
+        }
+        if(categoriaSeleccionada==="junior"){
+            let cuenta = (valorTicket*descJr)*cantidadSeleccionada;
+            divTotalTickets.innerHTML = `<h2>Total de tickets: $${cuenta}</h2>`;
+        }
     }
-}
+    else{
+        alert("Ingrese una cantidad válida")
+    }
+    }
+
 
 btnSubmit.addEventListener('click',(e)=>{
     e.preventDefault();
